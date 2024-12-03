c8af31b0432f49e8171905fa7b7b70ccd997a124
Hyebin Park <136055426+miloupark@users.noreply.github.com>
2024-12-04 01:57:49 +0900

    [level 0] Title: 양꼬치, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ JavaScript/프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,8 +1,8 @@
 function solution(n, k) {
     let answer = 0;
-    let costLamb = n * 12000; // 양꼬치 가격
-    let freeDrink = Math.floor(n / 10); // 서비스 음료수   
-    let costDrink = (k - freeDrink) * 2000; // 음료수 가격
+    const costLamb = n * 12000; // 양꼬치 가격
+    const freeDrink = Math.floor(n / 10); // 서비스 음료수   
+    const costDrink = (k - freeDrink) * 2000; // 음료수 가격
     
     answer = costLamb + costDrink // 총액
     
