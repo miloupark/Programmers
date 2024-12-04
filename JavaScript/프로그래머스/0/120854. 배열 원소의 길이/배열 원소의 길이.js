e7156bf48a1933934d6c649156a5179411ce923a
Hyebin Park <136055426+miloupark@users.noreply.github.com>
2024-12-04 20:10:42 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 32.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ JavaScript/프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,12 +1,15 @@
 function solution(strlist) {
+    const answer = [];
     
-    return strlist.map(el => el.length)
+    for (let i = 0; i < strlist.length; i++ ) {
+        answer.push(strlist[i].length);
+    }
+    
+    return answer;  
 }
 
-//     const answer = [];
-    
-//     for (let i = 0; i < strlist.length; i++ ) {
-//         answer.push(strlist[i].length);
-//     }
-    
-//     return answer;
+// function solution(strlist) {
+//     return strlist.map(el => el.length)
+// }
+
+// * map은 배열 strlist의 각 요소의 길이를 담은 새로운 배열을 반환 
