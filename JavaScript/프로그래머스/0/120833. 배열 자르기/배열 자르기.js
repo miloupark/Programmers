fc75d64b8159fb4e587ac567b344936c90f91145
Hyebin Park <136055426+miloupark@users.noreply.github.com>
2024-12-04 21:02:25 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120833. 배열 자르기/배열 자르기.js
+++ JavaScript/프로그래머스/0/120833. 배열 자르기/배열 자르기.js
@@ -4,3 +4,4 @@ function solution(numbers, num1, num2) {
 }
 
 // slice() 메서드는 끝 인덱스는 포함하지 않기 때문에 + 1로 마지막 인덱스 포함
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
