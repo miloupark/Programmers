06fef9accec3ec53bca8ce3532d505bea7c5f16d
Hyebin Park <136055426+miloupark@users.noreply.github.com>
2024-12-05 20:05:49 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ JavaScript/프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,10 +1,10 @@
-function solution(numbers) {
-    numbers.sort(function (a, b) {
-        return b - a;
-    });
+// function solution(numbers) {
+//     numbers.sort(function (a, b) {
+//         return b - a;
+//     });
     
-    return numbers[0] * numbers[1];
-}
+//     return numbers[0] * numbers[1];
+// }
 
 // sort 배열을 내림차순으로 정렬한 뒤 최댓값들을 곱해주는 방법
 //https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
@@ -19,8 +19,25 @@ function solution(numbers) {
 //   return max1 * max2;
 // }
 
+// 시도해본 풀이 수정본
+function solution(numbers) {
+  const max1 = Math.max(...numbers); 
+  const idx = numbers.indexOf(max1)
+  numbers[idx] = 0;
+    
+  const max2 = Math.max(...numbers); 
+    
+  return max1 * max2;
+}
+
 // Math.max 최댓값
 // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Math/max
 
 // filter() 메서드로 배열에서 최댓값 삭제
 // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+
+// indexOf
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+
+// reduce 
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
