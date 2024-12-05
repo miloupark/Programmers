36ac2096272d1fa2c84a76a7ded62ec3721a1512
Hyebin Park <136055426+miloupark@users.noreply.github.com>
2024-12-05 23:06:21 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,17 @@
+function solution(num_list) {
+    let even = 0; // 짝수의 개수
+    let odd = 0; // 홀수의 개수
+    
+    num_list.forEach(num => {
+        if (num % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    })
+    
+    return [even, odd];
+}
+
+// forEach() 메서드: 각 배열 요소에 대해 제공된 함수를 한 번씩 실행
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
