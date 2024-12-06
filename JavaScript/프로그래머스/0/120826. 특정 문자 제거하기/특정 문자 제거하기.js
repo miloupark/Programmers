6cf84a3e18585eac62475a01700f0751a5c79578
Hyebin Park <136055426+miloupark@users.noreply.github.com>
2024-12-06 18:28:44 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -0,0 +1,12 @@
+function solution(my_string, letter) {
+  const answer = my_string.replaceAll(letter, '');
+
+  return answer;
+}
+
+
+// replace 
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+
+// replaceAll
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/String/replaceAll
