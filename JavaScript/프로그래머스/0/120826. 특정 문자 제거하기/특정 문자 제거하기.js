afd096908a42f5b2fe8b9a9af21ec01ae06a0972
Hyebin Park <136055426+miloupark@users.noreply.github.com>
2024-12-06 18:34:34 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ JavaScript/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,5 +1,5 @@
 function solution(my_string, letter) {
-  const answer = my_string.replaceAll(letter, '');
+  const answer = my_string.replaceAll(letter, ''); // letter와 일치하는 모든 부분을 빈 문자열로 대체
 
   return answer;
 }
@@ -10,3 +10,7 @@ function solution(my_string, letter) {
 
 // replaceAll
 // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/String/replaceAll
+
+// *오류
+// replace(): 문자열 내에서 첫 번째로 발견된 일치하는 부분만 대체
+// 여러 번 나타나는 letter를 모두 제거하려면 반복문 or 정규표현식 활용해야함
