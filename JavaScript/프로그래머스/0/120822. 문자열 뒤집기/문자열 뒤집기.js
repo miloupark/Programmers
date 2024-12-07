69f50f60cb1df441f8725e52c65ccf1699f19637
Hyebin Park <136055426+miloupark@users.noreply.github.com>
2024-12-07 20:00:12 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ JavaScript/프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -4,10 +4,6 @@ function solution(my_string) {
     return words;
 }
 
-// reverse() 메서드는 배열의 요소 순서를 반대로 뒤집는 메서드, 문자열에는 바로 적용할 수 없음. 그래서 문자열을 먼저 배열로 변환한 뒤 reverse()를 호출하고, 다시 문자열로 변환
-// Array.prototype.reverse() O
-// String.prototype.reverse() X
-
 // reverse() 
 // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
 
